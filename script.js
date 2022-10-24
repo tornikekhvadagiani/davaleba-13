e2af055a781ec2d34d3b507966edf41e8fdd88bf
tornikekhvadagiani <113547924+tornikekhvadagiani@users.noreply.github.com>
2022-10-24 17:45:43 +0400

    dasasruli

---
+++ script.js
@@ -0,0 +1,17 @@
+let number = 5;
+let string = "tornike";
+let booleanTrue = true;
+let booleanFolse = false;
+let myUndefined;
+let myNull = null;
+console.log(number, string, booleanTrue, booleanFolse, myUndefined, myNull, );
+let myName = "tornike";
+let myHobby = "dance";
+let yearOfBirth = 1998;
+let currentYear  = 2022;
+const proposal = `my name is ${myName}, I am ${currentYear-yearOfBirth} old and my hobby is ${myHobby}.`;
+console.log(proposal);
+
+
+
+
